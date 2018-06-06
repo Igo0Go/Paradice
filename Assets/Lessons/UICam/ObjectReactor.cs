81a085a4f1dff209dd0183ca38a04a6428c9de12
LAPTOP-V4S5R4B7\ИгоGo <chudakin98@gmail.com>
2018-06-06 21:28:51 +0700

    Продолжена работа над уроком 5 (UI)

---
+++ Assets/Lessons/UICam/ObjectReactor.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ObjectForMission
+{
+    RedKey,
+    GreenKey,
+    YellowKey
+}
+
+public class ObjectReactor : MonoBehaviour {
+
+    public string Message;
+    public ObjectForMission Key;
+
+    private Animator _anim;
+
+	// Use this for initialization
+	void Start () {
+        _anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Action(RelictusController relictus)
+    {
+        relictus.GetSpecKey(Key);
+        _anim.SetBool("Active", true);
+        Message = string.Empty;
+    }
+}
--- Assets/Lessons/UICam/RelictusController.cs
+++ Assets/Lessons/UICam/RelictusController.cs
@@ -5,7 +5,11 @@ using UnityEngine.UI;
 
 public class RelictusController : MonoBehaviour {
 
+    public List<ObjectForMission> Keys;//sad
+    public List<GameObject> KeysImages;
     public Animator Connect;
+    public Text InterfaceText;
+    public Text MissionText;
     public Slider Energy;
     public float Speed;
     public float RotateSpeed;
@@ -17,13 +21,19 @@ public class RelictusController : MonoBehaviour {
     private Vector3 _moveVector;
     private float _rotationX;
     private float _rotationY;
-    //sad
+
     void Start () {
+        MissionText.text = "Доберитесь до медецинского отсека";
+        InterfaceText.text = string.Empty;
         Energy.value = 100;
         EnergySpeed = 1;
         _controller = GetComponent<CharacterController>();
         _rotationX = _rotationY = 0;
         Cursor.lockState = CursorLockMode.Locked;
+        foreach(var c in KeysImages)
+        {
+            c.SetActive(false);
+        }
     }
 	
 	void Update () {
@@ -71,6 +81,11 @@ public class RelictusController : MonoBehaviour {
             other.GetComponent<Animator>().SetBool("Active", true);
             EnergySpeed = 2;
         }
+        if (other.tag.Equals("Target"))
+        {
+            Connect.SetBool("Active", true);
+            InterfaceText.text = other.GetComponent<ObjectReactor>().Message;
+        }
     }
 
     private void OnTriggerExit(Collider other)
@@ -80,6 +95,48 @@ public class RelictusController : MonoBehaviour {
             Connect.SetBool("Active", false);
             other.GetComponent<Animator>().SetBool("Active", false);
             EnergySpeed = 1;
+        }
+        if (other.tag.Equals("Target"))
+        {
+            Connect.SetBool("Active", false);
+            InterfaceText.text = string.Empty;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag.Equals("Target"))
+        {
+            if(Input.GetKeyDown(KeyCode.E))
+            {
+                other.GetComponent<ObjectReactor>().Action(this);
+            }
+        }
+    }
+
+    public void GetSpecKey(ObjectForMission key)
+    {
+        InterfaceText.text = string.Empty;
+        switch (key)
+        {
+            case ObjectForMission.GreenKey:
+                {
+                    KeysImages[0].SetActive(true);
+                    Keys.Add(key);
+                    break;
+                }
+            case ObjectForMission.YellowKey:
+                {
+                    KeysImages[1].SetActive(true);
+                    Keys.Add(key);
+                    break;
+                }
+            case ObjectForMission.RedKey:
+                {
+                    KeysImages[2].SetActive(true);
+                    Keys.Add(key);
+                    break;
+                }
         }
     }
 
