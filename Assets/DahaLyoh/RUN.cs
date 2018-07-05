7c94b5828ef58a9a98ff29e7d8ade67dd4d8c2ee
LAPTOP-V4S5R4B7\ИгоGo <chudakin98@gmail.com>
2018-07-05 16:35:12 +0700

    персонаж с камерой (для Дарьи и Лёхи)

--- Assets/DahaLyoh/RUN.cs
+++ Assets/DahaLyoh/RUN.cs
@@ -3,36 +3,70 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RUN : MonoBehaviour {
+
+    public GameObject TargetPoint;
+    public GameObject AttackSphere;
     public GameObject Cam;
+    public LayerMask Obstacles;
+    public LayerMask NoPlayer;
     public float Speed;
     public float CamSpeed;
+    public float sdt;//asd
+    public float JumpForce;
+    public float Grav;
+    public float CamDistanceHorizontal
+    {
+        get {return _camDistanceHorizontal; }
+        set
+        {
+            _camDistanceHorizontal = value;
+            GetDistance();
+        }
+    }
+
+    private float _camDistanceHorizontal;
+    
     private CharacterController _controller;
     private Vector3 _moveVector;
-    private float _camDistance;
-    public float Grav;
-    public float JumpForce;
+    private Vector3 _camTarget;
+    private Vector3 _camVector;
+    public float _camDistance;
+
     private float _vertSpeed;
-    private float _speed;
-    // Use this for initialization
+   
     void Start () {
-        _controller = this.GetComponent<CharacterController>();
-        _speed = Speed;
+        AttackSphere.SetActive(false);
+        _camTarget = transform.position;
+        _controller = GetComponent<CharacterController>();
+        GetDistance();
+        CamTarget();
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    private void LateUpdate()
+    {
+        CamMoveToTarget();
+        CamObstaclesReactions();
+        Cam.transform.LookAt(TargetPoint.transform.position);
+    }
+
+    void Update ()
+    {
+        CamDistanceHorizontal = sdt;
         Move();
+
+        if(Input.GetKeyDown(KeyCode.K))
+        {
+            AttackSphere.SetActive(true);
+            Invoke("FinalAttack", 0.5f);
+        }
 	}
+
     private void Move()
     {
         if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
         {
             var x = Input.GetAxis("Horizontal");
             var z = Input.GetAxis("Vertical");
-            if(z != 0)
-            {
-                CamMove();
-            }
             _moveVector = Cam.transform.right * x + Cam.transform.forward * z;
             _moveVector = new Vector3(_moveVector.x, 0, _moveVector.z);
             transform.forward = _moveVector;
@@ -47,40 +81,64 @@ public class RUN : MonoBehaviour {
             }
         }
         _vertSpeed += Grav * Time.deltaTime;
-        _moveVector = new Vector3(_moveVector.x * _speed * Time.fixedDeltaTime, _vertSpeed * Time.deltaTime, _moveVector.z * _speed * Time.fixedDeltaTime);
+        _moveVector = new Vector3(_moveVector.x * Speed * Time.fixedDeltaTime, _vertSpeed * Time.deltaTime, _moveVector.z * Speed * Time.fixedDeltaTime);
         if (_moveVector != Vector3.zero)
         {
+            CamTarget();
             _controller.Move(_moveVector);
         }
     }
 
-    private void CamMove()
+    private void CamTarget()
     {
-        float distance = Vector3.Distance(Cam.transform.position, transform.position);
-        Vector3 lookVector = transform.position - Cam.transform.position;
-        if (distance <_camDistance)
+        _camTarget = TargetPoint.transform.position;
+        _camTarget = new Vector3(_camTarget.x, transform.position.y + 3, _camTarget.z);
+        _camVector = _camTarget - Cam.transform.position;
+        //_camVector = new Vector3(_camVector.x, 0, _camVector.z);
+        _camTarget += _camVector.normalized * -CamDistanceHorizontal;
+    }
+
+    private void CamMoveToTarget()
+    {
+        if(Cam.transform.position != _camTarget)
         {
-            float stepCam = CamSpeed * Time.deltaTime;
-            if(stepCam < _camDistance)
+            _camVector = _camTarget - Cam.transform.position;
+            float step = Speed * Time.fixedDeltaTime;
+            float distance = Vector3.Distance(_camTarget, Cam.transform.position);
+            if(step < distance)
             {
-                Cam.transform.position += lookVector.normalized * stepCam;
+                Cam.transform.position += _camVector.normalized * step;
             }
             else
             {
-                Cam.transform.position = transform.position - lookVector.normalized * _camDistance;
+                Cam.transform.position = _camTarget;
             }
         }
-        if (distance > _camDistance)
+    }
+
+    private void CamObstaclesReactions()
+    {
+        float distance = Vector3.Distance(Cam.transform.position, TargetPoint.transform.position);
+        RaycastHit hit;
+        if(Physics.Raycast(TargetPoint.transform.position, Cam.transform.position - TargetPoint.transform.position, out hit, distance ,Obstacles))
         {
-            float stepCam = CamSpeed * Time.deltaTime;
-            if (stepCam > _camDistance)
-            {
-                Cam.transform.position -= lookVector.normalized * stepCam;
-            }
-            else
-            {
-                Cam.transform.position = transform.position + lookVector.normalized * _camDistance;
-            }
+            Cam.transform.position = hit.point;
+            Cam.transform.position = new Vector3(Cam.transform.position.x, TargetPoint.transform.position.y, Cam.transform.position.z);
         }
+        else if(distance < _camDistance && !Physics.Raycast(Cam.transform.position, -Cam.transform.forward, 0.1f, Obstacles))
+        {
+            
+            CamMoveToTarget();
+        }
+    }
+
+    private void GetDistance()
+    {
+        _camDistance = Mathf.Sqrt(Mathf.Pow(CamDistanceHorizontal, 2) + Mathf.Pow(Cam.transform.position.y - TargetPoint.transform.position.y, 2));
+    }
+
+    private void FinalAttack()
+    {
+        AttackSphere.SetActive(false);
     }
 }
