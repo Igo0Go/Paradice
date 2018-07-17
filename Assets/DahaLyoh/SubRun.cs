cbb994af5f95bbc9cdd82852e96d58b09bb33089
DESKTOP-6F8Q8M6\Менем <menem8888@gmail.com>
2018-07-17 17:25:28 +0700

    НАвмеш

---
+++ Assets/DahaLyoh/SubRun.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SubRun : MonoBehaviour
+{
+	private CharacterController _NMA;
+	public Transform Glavtrans;
+
+	public NavMeshPath navPath;
+	
+	void Start () {
+		_NMA = GetComponent<CharacterController>();
+		navPath = new NavMeshPath();
+	}
+	
+	
+	
+	// Update is called once per frame
+	void Update () {
+		if (Vector3.Distance(transform.position, Glavtrans.position) > 0.5f )
+		{
+			MoveToGlav();
+			Gizmos.DrawLine(transform.position.normalized, GetDistinationNacMech(Glavtrans.position.normalized));
+		}
+		else
+		{
+			
+		}
+	}
+
+	private void MoveToGlav()
+	{
+		_NMA.Move((Glavtrans.position-transform.position).normalized*5f*Time.deltaTime);
+
+
+	}
+
+	public Vector3 GetDistinationNacMech(Vector3 request)
+	{
+		NavMeshHit myHitik;
+		if (NavMesh.SamplePosition(request, out myHitik, 100, NavMesh.AllAreas))
+		{
+			NavMesh.CalculatePath(transform.position, myHitik.position, NavMesh.AllAreas, navPath);
+			if (navPath != null && navPath.corners.Length > 1)
+			{
+				return navPath.corners[1];
+			}
+		}
+
+		return myHitik.position;
+	}
+}
